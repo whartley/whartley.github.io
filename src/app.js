d77def0c4652dbb94ab0b1abf349b12f2e3846e8
Woodie Hartley <woohart02@gmail.com>
2023-01-21 21:16:55 -0600

    Updates

--- src/app.js
+++ src/app.js
@@ -7,23 +7,26 @@ function toggleBtn(){
 
 hambugerBtn.addEventListener('click', toggleBtn);
 
-
-//send message to gmail
+//Send email with EmailJs
 function sendEmail(){
-    let name=document.getElementById("name");
-    let email=document.getElementById("email");
-    let subject=document.getElementById("subject");
-    let text=document.getElementById("text-area"); 
-    Email.send({
-        SecureToken: "32b3e27e-a111-4d0c-b2d5-dd895e7e2655",
-        To : "woohart02@gmail.com",
-        From : email,
-        Subject : subject,
-        Body : "Name: " + name.value
-        + "<br/> Email: " + email.value 
-        + "<br/> Subject: " + subject.value 
-        + "<br/> Message: " + text.value
-    }).then(
-      message => alert("Message Sent Successfully")
-    );
+    const serviceID = 'service_i7l8bnv';
+    const templateID = 'template_yrxjlo2'
+    var tempParams = {
+        from_name: document.getElementById('name').value,
+        to_name: 'Woodie',
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('text-area').value
+    }
+    
+emailjs
+    .send(serviceID,templateID,tempParams)
+    .then((res) => {
+        document.getElementById('name').value,
+        document.getElementById('email').value,
+        document.getElementById('subject').value,
+        document.getElementById('text-area').value
+        alert("Message Sent Successfully");
+        console.log(res, res.status, res.text);
+    }).catch((err) => {console.log(JSON.stringify(err));});
 }
